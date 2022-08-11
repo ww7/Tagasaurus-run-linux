2cb7b481c08b1557378eeeac7d0cecd91e5ecc7e
ww7 <vusatyis@gmail.com>
2022-08-11 04:11:06 +0300

    findmnt filter

--- tagasaurus-run-test.sh
+++ tagasaurus-run-test.sh
@@ -54,6 +54,7 @@ for devidusb in /dev/disk/by-id/usb*; do
       else 
         # Remount with `rw,uid=$(id -u),gid=$(id -g),utf8` and run Tagasaurus.
         if [[ -n $(findmnt -t vfat,exfat -O noexec -O showexec -nr -o target -S "$usbdev" | sed 's/\\x20/ /g') ]]; then
+          if [[ -n $(echo "$PWD" | grep "$usbmnt") ]]; then cd ~; fi
           remount_fat $usbdev $usbmnt
           echo "Running $path_ts"
           $path_ts
@@ -75,6 +76,7 @@ for devidusb in /dev/disk/by-id/usb*; do
         # Remount with with `rw,uid=$(id -u),gid=$(id -g),utf8`, then ask for download Tagasaurus and run.
         if [[ -n $(findmnt -t vfat,exfat -O noexec -O showexec -nr -o target -S "$usbdev" | sed 's/\\x20/ /g') ]]; then
           echo "Drive $usbmnt not allowed to exec."
+          if [[ -n $(echo "$PWD" | grep "$usbmnt") ]]; then cd ~; fi
           remount_fat $usbdev $usbmnt
           ts_download "$usbmnt"
           path_ts=$(find "$usbmnt" -maxdepth 2 -type f -iname "tagasaurus")
